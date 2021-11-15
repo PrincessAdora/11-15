e3e838c0ed5c24229a89c7a0805d148321aaf1bb
PrincessAdora <89851664+PrincessAdora@users.noreply.github.com>
2021-11-15 12:34:25 -0500

    11-15 work

--- src/App.js
+++ src/App.js
@@ -1,17 +1,34 @@
-import React from 'react';
+import React from "react";
 // Be sure to uncomment the TableRow component import before using a TableRow
 // import TableRow from './components/TableRow.js';
-import TableHeader from './components/TableHeader.js';
-import Navbar from './components/Navbar.js';
+import TableHeader from "./components/TableHeader.js";
+import Navbar from "./components/Navbar.js";
+import TableRow from "./components/TableRow.js";
 // Be sure to uncomment the line importing the data before using it!
-// import data from './knicks-data.js';
-import './App.css';
+import data from "./knicks-data.js";
+import "./App.css";
+
+console.log(data[0]);
 
 function App() {
+  let divStyle = {
+    backgroundColor: "rgb(0, 0, 255)",
+    minHeight: "100vh"
+  };
   return (
-    <div className="App">
-      <Navbar/>
-      <TableHeader/>
+    <div className="App" style={divStyle}>
+      <Navbar />
+      <TableHeader />
+      <TableRow
+        game={data[0].Game}
+        opponent={data[0].Opponent}
+        result={data[0].Result}
+        location={data[0].Location}
+        teamScore={data[0].TeamScore}
+        opponentScore={data[0].OpponentScore}
+        wins={data[0].W}
+        losses={data[0].L}
+      />
     </div>
   );
 }
--- src/components/TableRow.js
+++ src/components/TableRow.js
@@ -1,9 +1,18 @@
-import React from 'react';
-import './TableStyle.css';
+import React from "react";
+import "./TableStyle.css";
 
 function TableRow(props) {
-  return(
-    <div className="table-rows">
+  let rowStyle = {
+    backgroundColor: "rgb(150, 150, 150)"
+  };
+
+  if (props.result == "W") {
+    rowStyle.backgroundColor = "rgb(0, 255, 0)";
+  } else {
+    rowStyle.backgroundColor = "rgb(255, 0, 0)";
+  }
+  return (
+    <div className="table-rows" style={rowStyle}>
       <div>{props.game}</div>
       <div>{props.opponent}</div>
       <div>{props.result}</div>
